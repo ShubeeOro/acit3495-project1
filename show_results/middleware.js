7b309dccd92532e8e8c4b88f7360490f6ef05700
Kevin Ding <dingkevin2005@gmail.com>
2025-11-22 16:42:06 -0800

    Update middleware.js

--- show_results/middleware.js
+++ show_results/middleware.js
@@ -9,7 +9,7 @@ async function authenticateToken(req, res, next) {
 
     if (!token) {
         console.log("No token found. Redirecting to login...");
-        return res.redirect(`${AUTH_SERVICE_URL}/login`);
+        return res.redirect(`http://localhost:5001/login`);
     }
 
     try {
