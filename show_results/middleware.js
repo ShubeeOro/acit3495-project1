21375bc624876a324a367bf2f3d717f8f366e74d
Kevin Ding <dingkevin2005@gmail.com>
2025-12-03 13:15:53 -0800

    done I think

--- show_results/middleware.js
+++ show_results/middleware.js
@@ -9,7 +9,7 @@ async function authenticateToken(req, res, next) {
 
     if (!token) {
         console.log("No token found. Redirecting to login...");
-        return res.redirect(`http://auth_service:5001/login`);
+        return res.redirect(`${AUTH_SERVICE_URL}/login`);
     }
 
     try {
